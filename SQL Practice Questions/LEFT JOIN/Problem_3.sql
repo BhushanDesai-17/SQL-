56800fdea802c259c305c70b4ecb956090495834
Bhushan Desai <bhushandesai2006@gmail.com>
2026-06-20 14:13:20 +0530

    Add WHERE clause to filter for customers without orders

--- SQL Practice Questions/LEFT JOIN/Problem_3.sql
+++ SQL Practice Questions/LEFT JOIN/Problem_3.sql
@@ -3,4 +3,5 @@ SELECT Customers.customer_id,
       Customers.customer_name,
 FROM CUSTOMERS
 LEFT JOIN ORDERS
-ON Customers.customer_id = Orders.customer_id;
+ON Customers.customer_id = Orders.customer_id
+WHERE order_id IS NULL;
