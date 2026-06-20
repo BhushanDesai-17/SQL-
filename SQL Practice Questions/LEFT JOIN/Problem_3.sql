f03bd41abbffe47e897fda7beadb806397151146
Bhushan Desai <bhushandesai2006@gmail.com>
2026-06-20 14:12:32 +0530

    Add SQL query to find customers without orders
    Query to find customers without any orders.

---
+++ SQL Practice Questions/LEFT JOIN/Problem_3.sql
@@ -0,0 +1,6 @@
+-- Find customers who have not placed any order
+SELECT Customers.customer_id,
+      Customers.customer_name,
+FROM CUSTOMERS
+LEFT JOIN ORDERS
+ON Customers.customer_id = Orders.customer_id;
