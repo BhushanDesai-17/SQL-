055f115f987a44a03986912c3f48cee6bbce1e1f
Bhushan Desai <bhushandesai2006@gmail.com>
2026-07-02 19:19:53 +0530

    Delete SQL Practice Questions/LEFT JOIN/Problem_15.sql

--- SQL Practice Questions/LEFT JOIN/Problem_15.sql
+++
@@ -1,7 +0,0 @@
--- Last all users along with their payment dates (show null if no payment)
-SELECT Uers.user_id,
-      Users.user_name,
-      Payments.payment_date
-FROM USERS
-LEFT JOIN PAYMENTS
-ON Users.user_id = Payments.user_id;
